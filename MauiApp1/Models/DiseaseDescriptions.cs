5f36a3cf4f53ff9cdbcbfd3a121c03d1b223b6e8
ghostreact <diecity500@gmail.com>
2025-08-25 21:31:37 +0700

    update-mode-page

---
+++ MauiApp1/Models/DiseaseDescriptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1.Models
+{
+    public class DiseaseDescriptions
+    {
+        public string? Disease_Subcription { get; set; } //คำอธิบายโรค
+        public string? Disease_Cause { get; set; } //สาเหตุ
+        public string? Disease_Treatment { get; set; } //การรักษา
+        public string? Disease_Prevention { get; set; } //การป้องกัน
+    }
+}
