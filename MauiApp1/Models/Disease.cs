b9142bbd9cf704621224780873b36e6ef28305f1
Aekkaphon <nicklamphun@gmail.com>
2025-08-26 07:25:07 +0700

    update-content diseases

--- MauiApp1/Models/Disease.cs
+++ MauiApp1/Models/Disease.cs
@@ -6,8 +6,5 @@ namespace MauiApp1.Models
     {
         public string Name { get; set; } = "";
         public string? Symptom { get; set; } //อาการ
-        public string? Cause { get; set; } //สาเหตุ
-        public string? Treatment { get; set; } //การรักษา
-        public string? Prevention { get; set; } //การป้องกัน
     }
 }
